16b854f351c38d90d286acf23aac187dc5e57693
Jaye Hackett <jayehackett@Jayes-MacBook-Pro.local>
2020-03-28 23:02:57 +0000

    set up devise and authenticate dashboards

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,11 +6,12 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+    # before_action :authenticate_admin
+    before_action :authenticate_user!
 
-    def authenticate_admin
+    # def authenticate_admin
       # TODO Add authentication logic here.
-    end
+    # end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.

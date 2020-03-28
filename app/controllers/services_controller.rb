b543fb262cb30b8fd24962913a4bb28a027a6af0
Jaye Hackett <jayehackett@Jayes-MacBook-Pro.local>
2020-03-28 00:10:47 +0000

    recommended service functionality working-ish

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,16 +6,34 @@ class ServicesController < ApplicationController
 
     def index
         results = Geocoder.search(params[:postcode], region: "gb")
+
         if results.length > 0
+            @top_result = Service
+                .where("recommended = TRUE AND category && ARRAY[?]::varchar[]", params[:categories])
+                .limit(1)
+
             @result = results.first.formatted_address
             @coordinates = Geocoder.coordinates(params[:postcode])
-            if params[:categories]
-                @services = Service
-                .where("category && ARRAY[?]::varchar[]", params[:categories])
-                .near(results.first.coordinates, 200)
+
+            if @top_result.length > 0
+                if params[:categories]
+                    @services = Service
+                        .where("category && ARRAY[?]::varchar[] AND id != ?", params[:categories], @top_result[0].id)
+                        .near(results.first.coordinates, 200)
+                else
+                    @services = Service
+                        .where("id != ?", @top_result[0].id)
+                        .near(results.first.coordinates, 200)
+                end
             else
-                @services = Service.near(results.first.coordinates, 200)
-                # byebug
+                if params[:categories]
+                    @services = Service
+                        .where("category && ARRAY[?]::varchar[]", params[:categories])
+                        .near(results.first.coordinates, 200)
+                else
+                    @services = Service
+                        .near(results.first.coordinates, 200)
+                end
             end
         else
             redirect_to search_services_path, :notice => "Couldn't find any services near that location. Please make sure your location is a valid Camden area."

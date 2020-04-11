282a74261ae35a1cf1272dfa2cfc1d7c4377fa84
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-11 23:39:59 +0100

    add better seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Category.destroy_all
+Service.destroy_all
 
 Category.create([
     {
@@ -31,50 +32,27 @@ Category.create([
     }
 ])
 
+25.times do
+    Service.create(
+        name: FFaker::Company.name,
+        description: FFaker::Lorem.paragraph,
 
-# Service.destroy_all
+        street_address: FFaker::AddressUK.street_address,
+        postcode: ["S10 1UB", "W10 4AG", "NW10 3LL", "B33 8PH", "W1A 6US"].sample,
 
-# Service.create([
-#     {
-#         name: "Example service",
-#         description: "Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Jaguar shark! So tell me - does it really exist? I was part of something special. Hey, take a look at the earthlings. Goodbye! So you two dig up, dig up dinosaurs? Yes, Yes, without the oops!",
-#         key_point_1: "24/7 deliveries",
-#         key_point_2: "Good for older people",
-#         key_point_3: "Has an active social media presence",
-#         category: ["social", "food"],
-#         url: "#",
-#         postcode: "B33 8PP",
-#         latitude: 52.4775002727685,
-#         longitude: -1.8093153042122,
-#         recommended: true,
-#         how_to_contact: "Ask for Jan"
-#     },
-#     {
-#         name: "Second example service",
-#         description: "Goodbye! So you two dig up, dig up dinosaurs? Yes, Yes, without the oops!",
-#         key_point_1: "24/7 deliveries",
-#         key_point_2: "Good for older people",
-#         key_point_3: "Has an active social media presence",
-#         category: ["social"],
-#         phone: "0777 777 7777",
-#         postcode: "B33 8PP",
-#         latitude: 52.4775002727685,
-#         longitude: -1.8093153042122,
-#         how_to_contact: "Post on the Facebook group"
-#     },
+        url: FFaker::Internet.http_url,
+        email: FFaker::Internet.email,
+        phone: FFaker::PhoneNumber.phone_number,
 
-#     {
-#         name: "Third example service",
-#         description: "Jaguar shark! So tell me - does it really exist? I was part of something special. Hey, take a look at the earthlings. Goodbye! So you two dig up.",
-#         key_point_1: "24/7 deliveries",
-#         key_point_2: "Good for older people",
-#         key_point_3: "Has an active social media presence",
-#         category: ["social"],
-#         url: "#",
-#         phone: "0777 777 7777",
-#         postcode: "s10 2ll",
-#         latitude: 53.3733359351992,
-#         longitude: -1.50328085336585,
-#         how_to_contact: "Ask for Flora"
-#     },
-# ])
+        key_point_1: FFaker::Lorem.phrase,
+        key_point_2: FFaker::Lorem.phrase,
+        key_point_3: FFaker::Lorem.phrase,
+
+        how_to_contact: FFaker::Lorem.phrase,
+
+        recommended: FFaker::Boolean.maybe,
+        
+        published: true,
+        categories: [ Category.order("RANDOM()").first ]
+    )
+end

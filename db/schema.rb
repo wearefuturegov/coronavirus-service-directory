759b545313581aed826fa00c8d9d28fbbda893d7
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-11 23:39:35 +0100

    add street address field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_165106) do
+ActiveRecord::Schema.define(version: 2020_04_11_221909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_165106) do
     t.string "email"
     t.boolean "published", default: false, null: false
     t.string "admin_email"
+    t.string "street_address"
   end
 
   create_table "users", force: :cascade do |t|

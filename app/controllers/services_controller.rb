c910667c3436a30b107719736d239df5e82ce435
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-12 11:39:50 +0100

    fix front-end

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,15 +1,11 @@
 class ServicesController < ApplicationController
 
     def index
-
-        # byebug
-
         locations = Geocoder.search(params[:postcode], region: "gb") if params[:postcode]
         
         @services = Service.all.page(params[:page])
-
         @services = @services.near(locations.first.coordinates, 200) if locations && locations.length > 0
-        @services = @services.joins(:categories).where("categories.name in (?)", params[:categories]) if params[:categories]
+        @services = @services.joins(:categories).where("categories.name in (?)", params[:category]) if params[:category]
     end
 
     def new

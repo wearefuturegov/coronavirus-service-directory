341673b8de6e3f2817bfcfbd3fede3e9c51c4b21
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-14 21:11:40 +0100

    fix tests

--- spec/mailers/previews/service_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/service_mailer
-class ServiceMailerPreview < ActionMailer::Preview
-
-end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Service", type: :model do
+
+    before(:all) do 
+        Geocoder.configure(:lookup => :test)
+        Geocoder::Lookup::Test.add_stub(
+        "B33 8PP", [
+          {
+            'latitude'     => 40.7143528,
+            'longitude'    => -74.0059731,
+            'address'      => 'New York, NY, USA',
+            'state'        => 'New York',
+            'state_code'   => 'NY',
+            'country'      => 'United States',
+            'country_code' => 'US'
+          }
+         ]
+        )
+    end
     
     it "is not valid without a name" do
         expect(Service.new).to_not be_valid 
-        expect(Service.new(name: "Test service")).to be_valid
+        expect(Service.new(
+            name: "Test service",
+            description: "Test",
+            phone: "07777777777",
+            caretaker_phone: "07777777777"
+        )).to be_valid
     end
 
     it "it shouldn't be published by default" do

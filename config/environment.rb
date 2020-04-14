62fbf6d53255aad142ae3861f0fd54a0628a4d65
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-14 17:54:35 +0100

    try sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+    user_name: 'api_key',
+    password: ENV["SENDGRID_API_KEY"],
+    domain: 'wearefuturegov.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!

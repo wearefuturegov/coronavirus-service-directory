be8f5c72dc9fd5b215687b42914d3dcfab4d6af5
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-14 12:57:05 +0100

    add nicer sign-in routes and instructions for users on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :services, only: [:index, :new, :create]
 
   devise_for :users
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    get "register", to: "devise/registrations#new"
+  end
+
   namespace :admin do
     root to: "services#index"
     resources :services

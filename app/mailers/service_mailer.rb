e6ef63d2d4130d62087290d72c989b926c50ff8a
Jaye Hackett <jayehackett@Jayes-MacBook-Pro.local>
2020-04-16 22:02:08 +0100

    only registered admins get emails

--- app/mailers/service_mailer.rb
+++ app/mailers/service_mailer.rb
@@ -2,7 +2,7 @@ class ServiceMailer < ApplicationMailer
 
     def new_submission
         @service = params[:service]
-        User.all.each do |u|
+        User.where(admin: true).each do |u|
             mail(to: u.email, subject: 'New service awaiting review')
         end
     end
